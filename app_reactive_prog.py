3cdc3a8e7f49d036712e6a843ad0046280744953
Fabiano Soder <fabianosoder@hotmail.com>
2023-02-01 10:08:04 -0300

    added reactivex exemple

--- app_reactive_prog.py
+++ app_reactive_prog.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import cx_Oracle
+from reactivex import create
+
 
 userpwd = "APIMPINVANTARIO###2022" # Obtain password string from a user prompt or environment variable
 
@@ -10,11 +12,37 @@ connection = cx_Oracle.connect(user="API_MP_INVENTARIO", password=userpwd,
 app = Flask(__name__)
 
 
+@app.route('/rx', methods=['POST'])
+def rx():
+    source = create(push_tokens)
+    print("before")
+    source.subscribe(
+        on_next = lambda i: print("Received {0}".format(i)),
+        on_error = lambda e: print("Error Occurred: {0}".format(e)),
+        on_completed = lambda: print("Done!"),
+    )
+
+    print("after")
+    return jsonify({"success": True})
+
+def push_tokens(observer, scheduler):
+    observer.on_next("1VGTeO50GN6K")
+    observer.on_next("BaGQxlRcHJGduqCz")
+    observer.on_next("6uvaAY1EvY84")
+    observer.on_next("Ta7Woi5gxiMr")
+    observer.on_next("aUpWXkHV2ogJ")
+    observer.on_completed()
+
+
 @app.route('/', methods=['POST'])
 def login():
     data = request.get_json()
     token = data['token']
     
+    return get_seller_info(token)
+
+
+def get_seller_info(token):
     cursor = connection.cursor()
     sql = """
             SELECT 
@@ -30,7 +58,6 @@ def login():
     row = cursor.fetchone()
 
     return jsonify({'seller_id': row[0], 'status': row[1]})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
